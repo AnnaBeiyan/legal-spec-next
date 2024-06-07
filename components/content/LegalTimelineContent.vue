9ac0c2c39c7e61f4c7ca01c443f0138f263ab765
Anna Beiyan <131662099+AnnaBeiyan@users.noreply.github.com>
2024-06-07 15:43:07 +0800

    init

---
+++ components/content/LegalTimelineContent.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <hr />
+        <div class="timeline-middle">
+            <svg v-if="dotType==='fill_circle'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                <circle cx="10" cy="10" r="8" />
+            </svg>
+            <svg v-else-if="dotType==='hollow_circle'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" stroke="currentColor" class="h-5 w-5">
+                <circle cx="10" cy="10" r="8" fill="transparent" stroke-width="1"/>
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+            </svg>
+        </div>
+        <div class="timeline-end mb-5">
+            <time class="font-mono">{{date}}</time> <span v-if="author!=''" class="font-serif ml-4">{{ author }}</span>
+            <div class="text-lg font-black" v-if="title!==''||level!==''">{{title}} <span v-if="level!=''" class="badge">{{level}}</span></div>
+            <div><ContentSlot :use="$slots.default"/></div>
+        </div>
+        <hr />
+    </li>
+</template>
+<script setup lang="ts">
+import { computed, useSlots  } from "vue";
+
+const props = withDefaults(defineProps<{
+  date: string;
+  title?: string;
+  level?: string;
+  author?:string;
+  dotType?:"fill_circle" | "hollow_circle" | "none";
+}>(), {
+    title:"",
+    level:"",
+    author:"",
+    dotType:"fill_circle"
+});
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+    '@nuxthq/studio'
+  ],
+  // https://color-mode.nuxtjs.org
+  colorMode: {
+    preference: 'system', // default theme
+    dataValue: 'theme', // activate data-theme in <html> tag
+  },
+  // https://content.nuxtjs.org
+  content: {
+    documentDriven: true,
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: {
+        dark: 'github-dark',
+        default: 'github-light'
+      }
+    }
+  },
+})
